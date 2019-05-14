a67fff0af818ca3bd33e035fcfb1c8b64653a2bf
frankbahar <frankbahar07@gmail.com>
2019-05-14 00:34:51 -0400

    Top Menu update

--- src/main/java/com/frankbahar/base/TopMenu.java
+++ src/main/java/com/frankbahar/base/TopMenu.java
@@ -36,11 +36,11 @@ public class TopMenu {
 	public AccountsPage gotoAccounts() {
 		// driver.findElement(By.cssSelector("contains(text(),'Accounts')")).click();
 		
-		/*
-		 * driver.findElement(By.cssSelector(
-		 * "body.crmBodyWin:nth-child(2) div.newTopbar:nth-child(10) crm-menu.newMenuTable div.crmMenuLeft:nth-child(1) crm-tab:nth-child(5) div.lyteMenuItems:nth-child(2) div.lyteItem:nth-child(4) > a:nth-child(1)"
-		 * )).click();
-		 */ 
+		
+		// driver.findElement(By.cssSelector(
+		 //"body.crmBodyWin:nth-child(2) div.newTopbar:nth-child(10) crm-menu.newMenuTable div.crmMenuLeft:nth-child(1) crm-tab:nth-child(5) div.lyteMenuItems:nth-child(2) div.lyteItem:nth-child(4) > a:nth-child(1)"
+		 // )).click();
+		  
 		  Page.click("accountstab_CSS");
 		 		return new AccountsPage();
 	}
